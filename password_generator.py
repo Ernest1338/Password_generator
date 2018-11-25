0c7c31fd657418e5118e395aa791aab373759423
Ernest1338 <45213563+Ernest1338@users.noreply.github.com>
2018-11-25 23:20:41 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,22 +3,26 @@ import random
 characters=""
 passwo=""
 def gen():
-    global characters
-    global passwo
-    characters=int(many.get())
-    if characters<1 or characters>100:
+    try:
+        global characters
+        global passwo
+        characters=int(many.get())
+        if characters<1 or characters>100:
+            password.delete('1.0', END)
+            password.insert(END, "ERROR - Invalid number of characters.")
+        elif characters>0 and characters<101:
+            password.delete('1.0', END)
+            charact = ["q","Q","w","W","e","E","r","R","t","T","y","Y","u","U","i","I","o","O","p","P","a","A","s","S","d","D","f","F","g","G","h","H","j","J","k","K","l","L","z","Z","x","X","c","C","v","V","b","B","n","N","m","M","1","2","3","4","5","6","7","8","9","0"]
+            i=int(characters)
+            while i>=1:
+                i=i-1
+                passw = charact[random.randint(0,61)]
+                passwo = passwo + str(passw)
+            password.insert(END, passwo)
+            passwo=""
+    except:
         password.delete('1.0', END)
-        password.insert(END, "ERROR - Invalid number of characters.")
-    elif characters>0 and characters<101:
-        password.delete('1.0', END)
-        charact = ["q","Q","w","W","e","E","r","R","t","T","y","Y","u","U","i","I","o","O","p","P","a","A","s","S","d","D","f","F","g","G","h","H","j","J","k","K","l","L","z","Z","x","X","c","C","v","V","b","B","n","N","m","M","1","2","3","4","5","6","7","8","9","0"]
-        i=int(characters)
-        while i>=1:
-            i=i-1
-            passw = charact[random.randint(0,61)]
-            passwo = passwo + str(passw)
-        password.insert(END, passwo)
-        passwo=""
+        password.insert(END, "ERROR - Invalid characters")
 
 # Main Window
 root=Tk()
